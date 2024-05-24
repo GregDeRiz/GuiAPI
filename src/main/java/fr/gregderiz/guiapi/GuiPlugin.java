d94b64bca6b59af2994556bf215a4e88b5f5c05d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 01:50:34 +0200

    Testing jitpack version

--- src/main/java/fr/gregderiz/guiapi/GuiPlugin.java
+++ src/main/java/fr/gregderiz/guiapi/GuiPlugin.java
@@ -3,15 +3,13 @@ package fr.gregderiz.guiapi;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GuiPlugin extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
